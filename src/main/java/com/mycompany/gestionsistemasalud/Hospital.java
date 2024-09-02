de6702281304d25d9e503b750a763f37f0846d74
joaquin <joaquinllanosg@gmail.com>
2024-09-01 20:11:09 -0400

    Revert "segundo commit"
    This reverts commit 8850b2270848a20c8ff6f367aaf6de911c00dbde.

--- src/main/java/com/mycompany/gestionsistemasalud/Hospital.java
+++ src/main/java/com/mycompany/gestionsistemasalud/Hospital.java
@@ -8,51 +8,6 @@ package com.mycompany.gestionsistemasalud;
  *
  * @author joaquin
  */
-import java.util.*;
 public class Hospital {
-    private String nombreHospital;
-    private List<Medico> listaMedicos;
-    private HashMap<String, Pacientes> mapaPacientes;
-    private List<Cama> listaCamas;
-
-    //CONSTRUCT
-    public Hospital(String nombre){
-        nombreHospital = nombre;
-        listaMedicos = new ArrayList<>();
-        mapaPacientes = new HashMap<>();
-        listaCamas = new ArrayList<>();
-    }
-    //GETTER Y SETTER
-    public String getNombreHospital(){
-        return nombreHospital;
-    }
-    public void setNombreHospital(String nombre){
-        nombreHospital = nombre;
-    }
-
-    //METODOS
-    public void agregarMedico(Medico medico){
-        if(!listaMedicos.contains(medico))
-            listaMedicos.add(medico);
-    }
-    public void eliminarMedico(Medico medico){
-        if(listaMedicos.contains(medico))
-            listaMedicos.remove(medico);
-    }
-    public void agregarPaciente(String rut,Paciente paciente){
-        if(!mapaPacientes.containsKey(rut))
-            mapaPacientes.put(rut,paciente);
-    }
-    public void eliminarPaciente(String rut,Paciente paciente){
-        if(mapaPacientes.containsKey(rut))
-            mapaPacientes.remove(rut);
-    }
-    public void agregarCama(Cama cama){
-        if(!listaCamas.contains(cama))
-            listaCamas.add(cama);
-    }
-    public void eliminarCama(Cama cama){
-        if(listaCamas.contains(cama))
-            listaCamas.remove(cama);
-    }
+    
 }
